155902f208f451f8fabd8a580191699d8b65bc8c
devmariano <developer.mariano@gmail.com>
2023-04-19 09:31:28 -0300

    adicionado cadastro e validação de enfermeiro

---
+++ src/controllers/enfermeiro/cadastraEnfermeiro.js
@@ -0,0 +1,32 @@
+const Enfermeiro = require("../../models/enfermeiro");
+
+async function cadastraEnfermeiro(request, response) {
+    
+    try {
+        const dadosEnfermeiro = {
+            nome_completo: request.body.nome_completo,
+            genero: request.body.genero,
+            data_nascimento: request.body.data_nascimento,
+            cpf: request.body.cpf,
+            telefone: request.body.telefone,
+            instituicao_ensino_formacao: request.body.instituicao_ensino_formacao,
+            cofen_uf: request.body.cofen_uf
+        }
+        
+        const EnfermeiroExiste = await Enfermeiro.findOne({
+            where: {cpf: dadosEnfermeiro.cpf}
+        })
+
+        if(EnfermeiroExiste) 
+            return response.status(409).json({message: "Enfermeiro já cadastrado"})
+        
+        const novoEnfermeiro = await Enfermeiro.create(dadosEnfermeiro)
+        return response.status(201).json(novoEnfermeiro)
+
+    } catch (error) {
+        console.error('Requisição não processada', error.message)
+        response.status(400).json({message: "Requisição não processada"})
+    }
+}
+
+module.exports = cadastraEnfermeiro;
