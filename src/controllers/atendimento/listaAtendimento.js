4abc6c0bbbcc69f59128516bedbfe43b598a5175
devmariano <developer.mariano@gmail.com>
2023-04-20 23:27:24 -0300

    definiçoes de endpoint de listaAtendimentos

--- src/controllers/atendimento/listaAtendimento.js
+++ src/controllers/atendimento/listaAtendimento.js
@@ -1,45 +1,68 @@
 const Atendimento = require('../../models/atendimento');
+const Medico = require('../../models/medico')
+const Paciente = require('../../models/paciente')
 
 async function listaAtendimento(request, response) {
     try {
         const filtros = request.query // query param /api/atendimentos?medico=PARAMETRO 
-        
-        if(filtros.medico) {
 
-  
+        if (filtros.medico) {
+
+            const medico = await Medico.findByPk(filtros.medico);
+            if (!medico) {
+                return response
+                    .status(404)
+                    .json({ message: 'Médico não encontrado' });
+            }
+
             const atendimentos = await Atendimento.findAll(
                 {
                     where: {
                         medico_id: filtros.medico
                     }
                 }
-            ) 
+            )
+            if (atendimentos.length == 0) {
+                return response
+                    .status(404)
+                    .json({ message: 'Médico não realizou atendimentos' });
+            }
             response
-            .status(200)
-            .json(atendimentos) 
+                .status(200)
+                .json(atendimentos)
         } else if (filtros.paciente) {
-
+            const paciente = await Paciente.findByPk(filtros.paciente);
+            if (!paciente) {
+                return response
+                    .status(404)
+                    .json({ message: 'Paciente não encontrado' });
+            }
             const atendimentos = await Atendimento.findAll(
                 {
                     where: {
                         paciente_id: filtros.paciente
                     }
                 }
-            ) 
+            )
+            if (atendimentos.length == 0) {
+                return response
+                    .status(404)
+                    .json({ message: 'Paciente não teve atendimentos' });
+            }
             response
-            .status(200)
-            .json(atendimentos) 
-        }else {
-            const atendimentos = await Atendimento.findAll() 
+                .status(200)
+                .json(atendimentos)
+        } else {
+            const atendimentos = await Atendimento.findAll()
             response
-            .status(200)
-            .json(atendimentos) 
+                .status(200)
+                .json(atendimentos)
         }
 
     } catch (error) {
         return response
-        .status(500) 
-        .json({message: "Não foi possível processar a requisição"})
+            .status(500)
+            .json({ message: "Não foi possível processar a requisição" })
     }
 }
 
